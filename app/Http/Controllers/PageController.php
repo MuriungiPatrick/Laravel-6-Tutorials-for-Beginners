6e1afbce1d6959165d577fdfe0c625aef40f8dcb
Patrick Muriungi <muriungipatrick@gmail.com>
2019-08-06 21:30:56 +0300

    Route Groups

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,24 +9,24 @@ class PageController extends Controller
 /**
  * Get the Welcome view
  */
- public function index()
- {
-   return view('welcome');
- }
+     public function index()
+     {
+       return view('welcome');
+     }
 /**
  * Get the About us view
  */
 
- public function aboutus()
- {
-   return view('aboutus');
- }
+     public function aboutus()
+     {
+       return view('aboutus');
+     }
 
  /**
   * Get the Webdesign view
   */
- public function webdesign()
- {
-   return view('services.webdesign');
- }
+     public function webdesign()
+     {
+       return view('services.webdesign');
+     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,9 @@
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
+Route::prefix('page')->group(function() {
+  Route::get('/aboutus', 'PageController@aboutus')->name('about');//About us View
+  Route::get('/webdesign', 'PageController@webdesign')->name('services.webdesign');//Webdesign view
+});
 Route::get('/', 'PageController@index');//Welcome View
-Route::get('/aboutus', 'PageController@aboutus')->name('about');//About us View
-Route::get('/webdesign', 'PageController@webdesign')->name('services.webdesign');//Webdesign view
